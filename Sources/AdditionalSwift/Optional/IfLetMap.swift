e160c9346d86f2aec4c5f4bd325f18d0d0828f99
Stephen Martinez <enharmonicnuance@gmail.com>
2021-06-16 19:38:23 -0700

    Updated generic param names on IfLetMap type

--- Sources/AdditionalSwift/Optional/IfLetMap.swift
+++ Sources/AdditionalSwift/Optional/IfLetMap.swift
@@ -6,42 +6,42 @@
 //  Copyright © 2021 Stephen L. Martinez. All rights reserved.
 //
 
-public struct IfLetMap<Element, T, U, V> {
-    private let optional: () -> T?
-    private let map: ((Element) -> U) -> V
+public struct IfLetMap<Element, Wrapped, T, Container> {
+    private let optional: () -> Wrapped?
+    private let map: ((Element) -> T) -> Container
 
     public init(
-        _ optional: @escaping () -> T?,
-        map: @escaping ((Element) -> U) -> V
+        _ optional: @escaping () -> Wrapped?,
+        map: @escaping ((Element) -> T) -> Container
     ) {
         self.optional = optional
         self.map = map
     }
 
     public func `let` (
-        _ transform: @escaping (Element, T) -> U
-    ) -> ElseMap<Element, T, U, V> {
+        _ transform: @escaping (Element, Wrapped) -> T
+    ) -> ElseMap<Element, Wrapped, T, Container> {
         ElseMap(optional, let: transform, map: map)
     }
 }
 
 public extension IfLetMap {
-    struct ElseMap<Element, T, U, V> {
-        private let optional: () -> T?
-        private let letTransform: (Element, T) -> U
-        private let map: ((Element) -> U) -> V
+    struct ElseMap<Element, Wrapped, T, Container> {
+        private let optional: () -> Wrapped?
+        private let letTransform: (Element, Wrapped) -> T
+        private let map: ((Element) -> T) -> Container
 
         init(
-            _ optional: @escaping () -> T?,
-            `let` letTransform: @escaping (Element, T) -> U,
-            map: @escaping ((Element) -> U) -> V
+            _ optional: @escaping () -> Wrapped?,
+            `let` letTransform: @escaping (Element, Wrapped) -> T,
+            map: @escaping ((Element) -> T) -> Container
         ) {
             self.optional = optional
             self.letTransform = letTransform
             self.map = map
         }
 
-        public func `else`(_ transform: @escaping (Element) -> U) -> V {
+        public func `else`(_ transform: @escaping (Element) -> T) -> Container {
             optional()
                 .map { value in map { letTransform($0, value) } }
                 .coalesce(map(transform))
